8e0212480a9f61dbccd09d3bac1864c06a29298f
PettoDavida <maximilian.widman@gmail.com>
2022-12-21 22:24:36 +0100

    moved files

--- frontend/layouts/default.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="container mx-auto">
-    <Header />
-    <slot />
-  </div>
-</template>
--- frontend/nuxt.config.ts
+++
@@ -1,6 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  ssr: false,
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
-  css: ["@/assets/css/main.css"],
-})
