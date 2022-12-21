2c3b12baa9a08a5e46636018366a3e3ea17e2343
PettoDavida <maximilian.widman@gmail.com>
2022-12-21 16:21:58 +0100

    basic frontend with login and register

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="grid place-items-center">
+    <div v-for="sub in subs" :key="sub._id">
+      <NuxtLink :to="'/sub/' + sub._id"> {{ sub.title }}</NuxtLink>
+    </div>
   </div>
 </template>
+
 <script setup>
-const lol = 1
+const { data: subs } = useFetch("/api/sub", {
+  baseURL: "http://localhost:3001",
+})
 </script>
