e1350838c6f4af5e9957d54ba34059c439b3a0b2
Edwin Körner <edwin.korner@medieinstitutet.se>
2023-01-26 15:06:51 +0100

    Responsivity

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-row justify-around items-start">
+  <div class="flex flex-col justify-center items-center md:flex-row md:justify-around md:items-start">
     <div v-if="user" class="flex flex-col">
       <select
         class="my-2 block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
