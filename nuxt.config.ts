6a5bb3b5a96b6606f27a3c90f424feafbb5e3645
Maximilian Widman <42762656+PettoDavida@users.noreply.github.com>
2023-01-27 21:59:44 +0100

    Favicon (#6)
    * readme
    
    * Favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,11 @@ export default defineNuxtConfig({
   ssr: false,
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxtjs/supabase"],
   css: ["@/assets/css/main.css"],
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/images/favicon.ico" },
+      ],
+    },
+  },
 })
